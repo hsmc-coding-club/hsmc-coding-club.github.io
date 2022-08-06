6a1ba8e6072bea1414f6de812bbd51ed2187580d
sev <60162641+talkierbox@users.noreply.github.com>
2022-08-06 02:47:15 -0700

    Small Changes + More Effective Array Traversal

--- assets/showcase.js
+++ assets/showcase.js
@@ -1,8 +1,10 @@
-let cacImgs = [`gallery-1.jpg`, `gallery-2.jpg`, `gallery-3.png`, `gallery-4.png`, `gallery-5.jpg`]
+let cacImgs = [`gallery-1.jpg`, `gallery-2.jpg`, `gallery-3.png`, `gallery-4.png`, `gallery-5.jpg`];
 
-let i = 2;
+let cacIndex = 0;
 setInterval(() => {
-    if(i == cacImgs.length) i = 1;
-    $(`#cac-carousel`).attr(`src`, `assets/img/gallery/cac/${cacImgs[i]}`);
-    i++;
-}, 5000)
+    cacIndex++;
+    $(`#cac-carousel`).fadeToggle(1000, () => {
+        $(`#cac-carousel`).attr(`src`, `assets/img/gallery/cac/${cacImgs[cacIndex % cacImgs.length]}`);
+    });
+    $(`#cac-carousel`).fadeToggle(1000);
+}, 5000);
