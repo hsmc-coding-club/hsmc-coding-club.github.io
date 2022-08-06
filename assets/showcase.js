82d38c3a4adef23aa50b9c7518a3ddfa7b632ac6
sev <60162641+talkierbox@users.noreply.github.com>
2022-08-06 02:56:06 -0700

    add a photo

--- assets/showcase.js
+++ assets/showcase.js
@@ -1,10 +1,11 @@
-let cacImgs = [`gallery-1.jpg`, `gallery-2.jpg`, `gallery-3.png`, `gallery-4.png`, `gallery-5.jpg`];
+let cacImgs = [`gallery-1.jpg`, `gallery-2.jpg`, `gallery-3.png`, `gallery-4.png`, `gallery-5.jpg`, `gallery-6.jfif`];
 
 let cacIndex = 0;
 setInterval(() => {
     cacIndex++;
     $(`#cac-carousel`).fadeToggle(1000, () => {
         $(`#cac-carousel`).attr(`src`, `assets/img/gallery/cac/${cacImgs[cacIndex % cacImgs.length]}`);
+        cacIndex = cacIndex % cacImgs.length; // Save Memory Just in Case 
     });
     $(`#cac-carousel`).fadeToggle(1000);
 }, 5000);
