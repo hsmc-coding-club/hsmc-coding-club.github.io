7c9bcece08ae19e02e40a85d5db56f49e04cfc13
sev <60162641+talkierbox@users.noreply.github.com>
2022-08-22 09:07:07 -0700

    Longer fade time on image slideshow

--- assets/showcase.js
+++ assets/showcase.js
@@ -5,9 +5,9 @@ let cacImgs = [`gallery-1.jpg`, `gallery-2.jpg`, `gallery-3.png`, `gallery-4.png
 let cacIndex = 0;
 setInterval(() => {
     cacIndex++;
-    $(`#cac-carousel`).fadeToggle(500, () => {
+    $(`#cac-carousel`).fadeToggle(700, () => {
         $(`#cac-carousel`).attr(`src`, `assets/img/gallery/cac/${cacImgs[cacIndex % cacImgs.length]}`);
         cacIndex = cacIndex % cacImgs.length; // Save Memory Just in Case 
-        $(`#cac-carousel`).fadeToggle(500);
+        $(`#cac-carousel`).fadeToggle(700);
     });
 }, 5000);
